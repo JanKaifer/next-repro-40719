8935ee4db2610638c9c2e4103f9f88f44bb86612
Jan Kaifer <jan@kaifer.cz>
2022-12-04 11:49:44 +0100

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ export default function Page() {
     const sleep = (n: number) => new Promise(resolve => setTimeout(resolve, n))
     const scrollToTop = async () => {
         document.documentElement.style.scrollBehavior = "auto"
-        document.documentElement.getClientRects()
+        // document.documentElement.getClientRects()
         window.scrollTo(0, 0)
     }
 
